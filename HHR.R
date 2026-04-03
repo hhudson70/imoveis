e34ff51a1c4f1c03ff2ede82552661b2ceb6f5ed
hhudson70 <maiahenrique535@gmail.com>
2026-04-03 18:25:04 -0300

    commit analise

---
+++ HHR.R
@@ -0,0 +1,9 @@
+# --- Analise Estatistica de Imoveis ---
+# Leitura dos dados
+dados <- read.csv("imoveis - imoveis.csv")
+
+# 1. Analise Descritiva
+# Gera estatisticas como media, mediana e quartis para as 5 variaveis
+summary(dados)
+
+
